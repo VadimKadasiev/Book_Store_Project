af1cf2ee8109f89c9da0fe035b4ba01683ea7412
Vadim <kadasievva@gmail.com>
2024-02-22 01:18:09 +0200

    'solution 22/02'

--- src/main/java/book/store/dto/CreateBookRequestDto.java
+++ src/main/java/book/store/dto/CreateBookRequestDto.java
@@ -1,14 +1,23 @@
 package book.store.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull(message = "Title field can't be empty")
     private String title;
+    @NotNull(message = "Author field can't be empty")
     private String author;
+    @NotNull(message = "ISBN field can't be empty")
     private String isbn;
+    @NotNull(message = "Price field can't be empty")
+    @Min(value = 0,message = "Price must have a positive value")
     private BigDecimal price;
+    @NotNull(message = "Description field can't be empty")
     private String description;
+    @NotNull(message = "Cover_Image field can't be empty")
     private String coverImage;
 }
